39dfd85cf35f6dbf2a8f94a9676c001f5fe8350c
Naumanhassan307 <Naumanhassan307@gmail.com>
2021-08-21 23:42:25 -0700

    poster complete

--- src/foodLink/poster/Poster.js
+++ src/foodLink/poster/Poster.js
@@ -4,7 +4,11 @@ import "./PosterStyle.css";
 function Poster(){
     return (
       <div className="poster-main-div">
-        <h1 className="poster-main-div-txt">hello</h1>
+        <p className="poster-main-div-txt">
+          Order Tasty & Fresh <br /> Food.
+          <br />
+          <p className="poster-main-div-txt-1">anytime!</p>
+        </p>
       </div>
     );
 }
